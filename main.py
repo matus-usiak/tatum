637d474eaf85234848ffc1fdce458b4a3ed7981b
Matúš Ušiak <matusu@kentico.com>
2026-01-29 13:18:52 +0100

    balance and raw trxs

---
+++ main.py
@@ -0,0 +1,43 @@
+from decimal import Decimal, ROUND_DOWN
+
+import requests
+
+baseUrl = "https://api.tatum.io/v4"
+address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
+
+headers = {
+    "accept": "application/json",
+    "X-Api-Key": "t-69789496ace70350f2244903-a9afc48f474d429ca5f485a0",
+    "host": "api.tatum.io"
+}
+
+
+def get_wallet_balance(walletaddress):
+    url = f"{baseUrl}/data/wallet/balance/time?chain=ethereum-mainnet&addresses={walletaddress}&time=2099-01-01"
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    data = response.json()
+    balance = Decimal(data["result"][0]["balance"])
+    return balance.quantize(Decimal("0.00000000"), rounding=ROUND_DOWN)
+
+
+def get_latest_trx(walletaddress):
+    url = f"{baseUrl}/data/transaction/history?chain=ethereum-mainnet&addresses={walletaddress}&transactionTypes" \
+          f"=multitoken&sort=desc"
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def main():
+    balance = get_wallet_balance(address)
+
+    transactions = get_latest_trx(address)
+
+    print(f"Current balance: {balance} ETH")
+
+    print(f"Latest transactions: {transactions}")
+
+
+if __name__ == "__main__":
+    main()
